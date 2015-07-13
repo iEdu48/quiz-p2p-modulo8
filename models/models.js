afb8baeae240eb01f5d71b4d4a6c4a1b56f18ac7
iEdu48 <edugarfol@gmail.com>
2015-07-14 01:12:32 +0200

    quiz-p2p-modulo8
    Ejercicio P2P modulo8 para la inclusión del tema de cada pregunta

--- models/models.js
+++ models/models.js
@@ -26,7 +26,7 @@ var sequelize = new Sequelize(DB_name, user, pwd,
     }
 );
 
-// Importar definicion de la tabla Quiz
+// Importar definicion de la tabla  Quiz
 
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
