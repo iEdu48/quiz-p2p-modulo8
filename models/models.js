6cd09f734874a269cf54e0f31ca4211301e7fac0
iEdu48 <edugarfol@gmail.com>
2015-07-13 17:45:34 +0200

    quiz-p2p-modulo8
    Ejercicio P2P modulo8 para la inclusión del tema de cada pregunta

--- models/models.js
+++ models/models.js
@@ -35,12 +35,13 @@ exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
+
     // then(..) ejecuta el manejador una vez creada la tabla
     Quiz.count().then(function (count){
         if(count === 0) {   // la tabla se inicializa solo si está vacía
             Quiz.bulkCreate(
-                [   { pregunta: 'Capital de Italia',   respuesta: 'Roma' },
-                    { pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+                [   { pregunta: 'Capital de Italia',   respuesta: 'Roma', tema: 'otro' },
+                    { pregunta: 'Capital de Portugal', respuesta: 'Lisboa', tema: 'otro'}
                 ]
             ).then(function(){console.log('Base de datos inicializada')});
         };
